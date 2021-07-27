2af16a23d2a69d07ea77ee9e10ff0ed11204c52a
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-07-27 16:36:06 +0300

    changed the method of submitting the form, added navigation to the buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,14 @@ toggler.addEventListener('click', () => {
   sidebar.classList.toggle('headline--active');
   label.classList.toggle('nav__toggler--close');
 });
+
+const form = document.querySelector('.subscribe__form');
+const inputs = document.querySelectorAll('.subscribe__form-input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
